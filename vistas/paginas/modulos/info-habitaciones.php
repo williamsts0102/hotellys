6f4d663db997b3b33fa8c30a3fbf1cc0b2452886
klark14 <klarkmaickolp@gmail.com>
2023-10-15 22:23:44 -0500

    sesion 4
    sesion4

--- vistas/paginas/modulos/info-habitaciones.php
+++ vistas/paginas/modulos/info-habitaciones.php
@@ -252,36 +252,39 @@ INFO HABITACIÓN
 							$categorias = ControladorCategorias::ctrMostrarCategorias();
 						?>
 
-						<?php if ($_GET["pagina"] != $value["ruta"]): ?>
+						
 
 
 						<?php foreach($categorias as $key => $value): ?>
 
-							<div class="col-12 pb-3 px-0 px-lg-3">
+
+							<?php if ($_GET["pagina"] != $value["ruta"]): ?>
+							
+							<div class="col-12 col-lg-4 pb-3 px-0 px-lg-3">
 
 							<a href="<?php echo $ruta.$value["ruta"];  ?>">
-								
-								<figure class="text-center">
-									
-									<img src="<?php echo $servidor.$value["img"]; ?>" class="img-fluid" width="100%">
+	
+							<figure class="text-center">
+		
+								<img src="<?php echo $servidor.$value["img"]; ?>" class="img-fluid" width="100%">
 
-									<p class="small py-4 mb-0"><?php echo $value["descripcion"]; ?></p>
+								<p class="small py-4 mb-0"><?php echo $value["descripcion"]; ?></p>
 
-									<h3 class="py-2 text-gray-dark mb-0">DESDE $<?php echo number_format($value["continental_baja"]); ?> COP</h3>
+								<h3 class="py-2 text-gray-dark mb-0">DESDE $<?php echo number_format($value["continental_baja"]); ?> COP</h3>
 
-									<h5 class="py-2 text-gray-dark border">Ver detalles <i class="fas fa-chevron-right ml-2"></i></h5>
-									
-									<h1 class="text-white p-3 mx-auto w-50 lead text-uppercase" style="background:<?php echo $value["color"]; ?>"><?php echo $value["tipo"]; ?></h1>
+								<h5 class="py-2 text-gray-dark border">Ver detalles <i class="fas fa-chevron-right ml-2"></i></h5>
+		
+								<h1 class="text-white p-3 mx-auto w-50 lead text-uppercase" style="background:<?php echo $value["color"]; ?>"><?php echo $value["tipo"]; ?></h1>
 
-								</figure>
+							</figure>
 
 							</a>
 
 							</div>
 
-							<?php endif ?>
+						<?php endif ?>
 
-						<?php endforeach ?>
+					<?php endforeach ?>
 
 						</div>
 
