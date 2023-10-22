e59d7be4782cbb815d520d4269f1b83c14d9ea85
Wendycch <wendy.actitud11@gmail.com>
2023-10-22 17:19:46 -0500

    Update habitaciones.modelo.php

--- modelos/habitaciones.modelo.php
+++ modelos/habitaciones.modelo.php
@@ -6,7 +6,8 @@ class ModeloHabitaciones{
 
         static public function mdlMostrarHabitaciones($tabla1, $tabla2, $valor){
 
-        $stmt = conexion::conectar()->prepare("SELECT $tabla1.*, $tabla2.* FROM $tabla1 INNER JOIN $tabla2 ON $tabla1.id = $tabla2.tipo_h WHERE ruta = :ruta");
+        $stmt = conexion::conectar()->prepare("SELECT $tabla1.*, $tabla2.* FROM $tabla1 INNER 
+        JOIN $tabla2 ON $tabla1.id = $tabla2.tipo_h WHERE ruta = :ruta");
 
 
         $stmt -> bindParam(":ruta", $valor, PDO::PARAM_STR);       
